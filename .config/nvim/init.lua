cb917b461c9c6da8dced900971b66bbf72754215
experimentallife <user@experimentallife.dev>
2022-06-21 09:49:20 -0500

    :sparkles: toggle language

---
+++ .config/nvim/init.lua
@@ -0,0 +1,3 @@
+require "core"
+require "core.utils"
+require "core.options"
---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+local g = vim.g
+
+opt.clipboard = "unnamedplus"
+opt.cul = false
+
+opt.shiftwidth = 2
+
+opt.number = true
+opt.numberwidth = 2
+
