ff96799fae28be1fa2c8d34e35e9e898bfa6313e
experimentallife <user@experimentallife.dev>
2022-08-07 21:01:56 -0500

    :sparkles: change themes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,30 +3,21 @@
 # If all your bars have ipc enabled, you can use 
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
- # killall -q polybar
+killall -q polybar
 
 
 # Wait until the processes have been shut down
 # while pgrep -x polybar >/dev/null; do sleep 1; done
 #
-# screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
-#
-# if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
-#   MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
-# else
-#   primary=$(xrandr --query | grep primary | cut -d" " -f1)
-#
-#   for m in $screens; do
-#     if [[ $primary == $m ]]; then
-#         MONITOR=$m TRAY_POS=right polybar NPink &
-#     else
-#         MONITOR=$m TRAY_POS=none polybar secondary &
-#     fi
-#   done
-# fi
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log
-polybar main | tee -a /tmp/polybar1.log & disown
+echo "Bars starting..."
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
 
 echo "Bars launched..."
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 
+## Color paletter
+
+- ![#000000](https://img.shields.io/static/v1?label=&message=000000&color=000000) `Primary`
+- ![#D88F12](https://img.shields.io/static/v1?label=&message=D88F12&color=D88F12) `Secondary`
