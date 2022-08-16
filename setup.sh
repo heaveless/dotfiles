21fd547039ce05853d77239cb378988929945d3c
experimentallife <user@experimentallife.dev>
2022-08-16 01:18:15 -0700

    :sparkles: add wsl variant

--- setup.sh
+++ setup.sh
@@ -1,45 +1,6 @@
 #!/bin/bash
 
-config_files=`find . -maxdepth 1 -type f`
-config_folders=`find ./.config/ -mindepth 1 -maxdepth 1 -type d`
-config_direction=`realpath ~/`
+source ./winvars
 
-# linked config files
-
-for entry in $config_files 
-do
-  match=false
-
-  while read line;
-  do
-    if [ -z $line ]; 
-    then
-      break
-    fi
-
-    if [ $line -ef $entry ];
-    then
-      match=true
-      break
-    fi
-  done < .linkedignore
-
-  if ! [[ $match == true ]];
-  then
-    echo "Linked $config_direction${entry:1}";
-
-    rm -f "$config_direction${entry:1}";
-    ln -s "$(realpath $entry)" "$config_direction";
-  fi
- done
-
-# linked config folders
-
-for entry in $config_folders
-do 
-  echo "Linked $config_direction${entry:1}";
-
-  rm -rf "$config_direction${entry:1}";
-  ln -s "$(realpath $entry)" "$config_direction/.config/";
-done
-
+./linker.sh
+./winlinker.sh
